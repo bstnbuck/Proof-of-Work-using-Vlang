89ce175300d746d046a61183fb920c394bf70b28
bstnbuck <59334447+bstnbuck@users.noreply.github.com>
2020-07-01 10:06:16 +0200

    reformatted file, updated functions

--- main.v
+++ main.v
@@ -1,43 +1,36 @@
-import (
-        crypto.sha256
-        rand
-        time
-)
+import crypto.sha256
+import rand
+import time
 
 /*
 Because this is my first V-project, there is of course still a lot of potential upwards. 
 Care should be taken with the leading zeros, as they influence the runtime considerably. 
 There is no garbage collection (don't know how yet), so watch memory !!!
 */
-
 fn main() {
-	seed := time.now().unix				//seed rand with unix time
-	mut nonce := rand.rand_r(&seed)			//make new random nonce
-    	text := "HelloWorld"		
-	mut count := 0					//count counts the runs
-    	println("Random Nonce: "+nonce.str())
-	println("Started: "+time.now().str())
-        
-	//mut hash := sha256.new()			// [OLD] make new SHA256 hash
-	for{						//endless while
-		//hash.write((text+nonce.str()).bytes())	// [OLD] write the text and nonce into hash as bytes
-		hashthex := sha256.hexhash(text+nonce.str())	//instead using hexhhash which generates hash and convert the byte output into hex
-		
-		if hashthex.starts_with("00000"){
-			println('Hash found!: ' + hashthex + ' Text+Nonce: ' + text+nonce.str()+ ' Count: ' + count.str())	//print hash, text&nonce and counter
-			break			//stop the loop
+	mut nonce := rand.u32() // make new random nonce
+	mut count := 0 // count counts the runs
+	text := 'HelloWorld'
+	println('Random Nonce: ' + nonce.str())
+	println('Started: ' + time.now().str())
+	mut hashthex := ''
+	for { // endless while
+		hashthex = sha256.hexhash(text + nonce.str())
+		if hashthex.starts_with('0000') {
+			println('Hash found!: $hashthex  Text+Nonce: ' + text + nonce.str() + ' Count: ' +
+				count.str()) // print hash, text&nonce and counter
+			break // stop the loop
 		}
-		//println(hashthex)		// DEBUG	
-		
+		// println(hashthex)		// DEBUG
 		hashthex.free()
-		//hash.reset()			// [OLD] reset hash
-		nonce += 1			//increment nonce and counter by 1
-		count += 1
+		nonce++ // increment nonce and counter by 1
+		count++
 	}
-	println("Stopped "+time.now().str())	//print timestamp
+	println('Stopped ' + time.now().str()) // print timestamp
 }
+
 /*
-		Examples:
+Examples:
 
 		Random Nonce: 1473829389
 		Started: 2020-04-13 17:08:52
@@ -58,5 +51,4 @@ fn main() {
 		Started: 2020-04-13 17:31:36
 		Hash found!: 000000baf0d180206b2df7e5b1deb1b9f4324b43d04fd11bc8b592cf4e780e14 Text+Nonce: HelloWorld1283307941 Count: 721620
 		Stopped 2020-04-13 17:31:51
-
 */
